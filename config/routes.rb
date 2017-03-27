9718cd5bb76d7c09ccd831efb254048f411911f0
oseifrimpong <obedosei@gmail.com>
2017-03-28 05:39:18 +0800

    devise 101

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home/index'
 
-  root to: "home#index"
+  devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+    unauthenticated do
+      root 'devise/registrations#new', as: :unauthenticated_root
+    end
+  end
 end
